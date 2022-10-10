fca6039079fa273cb4dc56ec8dddb9f457d7b65b
Valdenir Severino <vald3nir@gmail.com>
2022-10-10 16:36:15 -0300

    Update address_model.dart

--- flutter_cep_app/lib/models/address_model.dart
+++ flutter_cep_app/lib/models/address_model.dart
@@ -1,35 +1,24 @@
+import 'dart:convert';
+
 class AddressModel {
   final String? cep;
   final String? logradouro;
   final String? complemento;
 
-  AddressModel(this.cep, this.logradouro, this.complemento);
+  AddressModel(
+      {required this.cep, required this.logradouro, required this.complemento});
 
-  // AddressModel.fromJson(Map<String, dynamic> json) {
-  //   cep = json['cep'];
-  //   logradouro = json['logradouro'];
-  //   complemento = json['complemento'];
-  //   bairro = json['bairro'];
-  //   localidade = json['localidade'];
-  //   uf = json['uf'];
-  //   ibge = json['ibge'];
-  //   gia = json['gia'];
-  //   ddd = json['ddd'];
-  //   siafi = json['siafi'];
-  // }
+  Map<String, dynamic> toMap() {
+    return {"cep": cep, "logradouro": logradouro, "complemento": complemento};
+  }
 
-  // Map<String, dynamic> toJson() {
-  //   final Map<String, dynamic> data = <String, dynamic>{};
-  //   data['cep'] = cep;
-  //   data['logradouro'] = logradouro;
-  //   data['complemento'] = complemento;
-  //   data['bairro'] = bairro;
-  //   data['localidade'] = localidade;
-  //   data['uf'] = uf;
-  //   data['ibge'] = ibge;
-  //   data['gia'] = gia;
-  //   data['ddd'] = ddd;
-  //   data['siafi'] = siafi;
-  //   return data;
-  // }
+  factory AddressModel.fromMap(Map<String, dynamic> map) {
+    return AddressModel(
+        cep: map["cep"],
+        logradouro: map["logradouro"],
+        complemento: map["complemento"]);
+  }
+
+  factory AddressModel.fromJson(String json) =>
+      AddressModel.fromMap(jsonDecode(json));
 }
